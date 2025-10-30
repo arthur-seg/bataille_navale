7b95bde1d47e173a2ff0fcdcd64ff995bfef5ea6
arthur-seg <arthur.seguette22@gmail.com>
2025-10-30 13:43:17 +0100

    methode affiche() de la classe Grille

--- grille.py
+++ grille.py
@@ -5,3 +5,10 @@ class Grille:
         self.vide = "~"
         self.tir = "x"
         self.liste = [self.vide for _ in range(n_lignes*n_colonnes)]
+
+    def affiche(self):
+        for idx, elt in enumerate(self.liste):
+            if idx % self.n_colonnes == 0 and idx != 0:
+                print("\n", end='')
+            print(elt, end='')
+        print("\n", end='')
--- test_grille.py
+++ test_grille.py
@@ -15,3 +15,24 @@ def test_liste_grille_vide():
 def test_liste_grille_general():
     g = Grille(2, 3)
     assert g.liste == ["~", "~", "~", "~", "~", "~"]
+
+
+def test_affiche_1(capfd):
+    g = Grille(1, 1)
+    g.affiche()
+    out, _ = capfd.readouterr()
+    assert out == "~\n"
+
+
+def test_affiche_colonne(capfd):
+    g = Grille(3, 1)
+    g.affiche()
+    out, _ = capfd.readouterr()
+    assert out == "~\n~\n~\n"
+
+
+def test_affiche_general(capfd):
+    g = Grille(3, 4)
+    g.affiche()
+    out, _ = capfd.readouterr()
+    assert out == "~~~~\n~~~~\n~~~~\n"
