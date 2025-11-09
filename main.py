bdf3fb45965d632723c4c9ca6232d25d9e73850d
arthur-seg <arthur.seguette22@gmail.com>
2025-11-09 03:08:08 +0100

    mise à jour des grilles après les tirs, affichage des bateaux coulés, condition de fin de partie

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ echec_ajout = 1
 succes_ajout = 0
 
 
+### fonctions ###
+def jeu_est_fini(grille):
+    for bateau in grille.bateaux_ajoutes:
+        if bateau.coule(grille) is False:
+            return False
+    return True
+
+
 print("---------------BATAILLE NAVALE---------------")
 reponse = input("appuyer sur s pour commencer ou q pour quitter\n")
 if reponse != 's':
     quit()
-while reponse != 'q':
+while 1:
     # init le jeu
+    nombre_coups = 0
     len_liste_grille = n_colonnes_grille * n_lignes_grille
     grille = Grille(n_lignes_grille, n_colonnes_grille)
     positions_possibles = PositionsPossibles(grille, list(dict_bateaux.values()))
@@ -32,24 +41,52 @@ while reponse != 'q':
         bateau.ligne = pos // n_colonnes_grille
         bateau.colonne = pos % n_colonnes_grille
         grille.ajoute(bateau)
+    grille_utilisateur = Grille(n_lignes_grille, n_colonnes_grille)  # grille affichée à l'utilisateur
     # boucle de gameplay jusqu'a la fin
-    grille_utilisateur = Grille(n_lignes_grille, n_colonnes_grille) # grille affichée à l'utilisateur
-    print(grille_utilisateur)
-    result = None
-    idx_tir = -1
-    while result is None or idx_tir < 0 or idx_tir >= len_liste_grille:
-        tir_utilisateur = input("où tirer, capitaine ?\nentrez la ligne puis la colonne\n")
-        if tir_utilisateur == 'q':
-            quit()
-        pattern = re.compile("\(?(\d+)[ ,;]+(\d+)\)?")
-        result = pattern.fullmatch(tir_utilisateur)
-        if result is None:
-            print("coordonnées incompréhensibles. Réessayez.")
-        else:
-            ligne_tir = result.group(1)
-            colonne_tir = result.group(2)
-            idx_tir = ligne_tir * n_colonnes_grille + colonne_tir
-            if idx_tir >= len_liste_grille:
-                print("ce tir est hors de la grille !")
+    jeu_fini = False
+    while not jeu_fini:
+        print(grille_utilisateur)
+        result = None
+        ligne_tir = -1
+        colonne_tir = -1
+        while result is None or ligne_tir < 0 or ligne_tir >= n_lignes_grille or \
+                colonne_tir < 0 or colonne_tir >= n_colonnes_grille:
+            tir_utilisateur = input("où tirer, capitaine ?\nentrez la ligne puis la colonne\n")
+            if tir_utilisateur == 'q':
+                quit()
+            pattern = re.compile("\(?(\d+)[ ,;]+(\d+)\)?")
+            result = pattern.fullmatch(tir_utilisateur)
+            if result is None:
+                print("coordonnées incompréhensibles. Réessayez.")
+            else:
+                ligne_tir = int(result.group(1))
+                colonne_tir = int(result.group(2))
+                if ligne_tir < 0 or ligne_tir >= n_lignes_grille or colonne_tir < 0 or colonne_tir >= n_colonnes_grille:
+                    print("ce tir est hors de la grille !")
+        nombre_coups += 1
+        grille.tirer(ligne_tir, colonne_tir)
+        grille_utilisateur.tirer(ligne_tir, colonne_tir)
+        touche = False
+        for bateau in grille.bateaux_ajoutes:
+            for position in bateau.positions:
+                if (ligne_tir, colonne_tir) == position:
+                    touche = True
+                    grille_utilisateur.tirer(ligne_tir, colonne_tir, '💣')
+                    if bateau.coule(grille):
+                        print("bateau adverse coulé !")
+                        grille_utilisateur.ajoute(bateau)
+                        jeu_fini = jeu_est_fini(grille)
+                    break
+                if touche:
+                    break
     # demander une nouvelle partie
-    reponse = input("?\n")    #### PAS ENLEVER
+    print(grille_utilisateur)
+    print("bravo ! vous avez détruit la flotte adverse en {} coups !".format(nombre_coups))
+    while reponse != 'o' and reponse != 'n':
+        reponse = input("prêt pour une nouvelle partie ? (o ou n)\n")
+        if reponse != 'o' and reponse != 'n' and reponse != 'q':
+            print("je n'ai pas compris votre réponse")
+        elif reponse == 'o':
+            print("c'est reparti !")
+        else:
+            quit()
